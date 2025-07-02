801dd704ef8305d2aaa64466be487d30e25f24a3
AlanGF7 <af189107@gmail.com>
2025-07-01 21:38:12 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+build_histogram = st.checkbox('Mostrar histograma')
+
+if build_histogram:
+    st.write('Creación de un histograma para la columna "odometer"')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+build_scatter = st.checkbox('Mostrar gráfico de dispersión')
+
+if build_scatter:
+    st.write('Creación de un gráfico de dispersión: "odometer" vs "price"')
+    fig2 = px.scatter(
+        car_data,
+        x="odometer",
+        y="price",
+        color="type",
+        hover_data=["model_year"]
+    )
+    st.plotly_chart(fig2, use_container_width=True)
